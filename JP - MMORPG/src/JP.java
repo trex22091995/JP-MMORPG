6249764a9489de814d367185ffe78b987bbb8c57
Peter <peter.esser@uhs-computer.de>
2014-12-18 22:07:48 +0100

    Init

---
+++ JP - MMORPG/src/JP.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Peter
+ *
+ */
+public class JP {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
