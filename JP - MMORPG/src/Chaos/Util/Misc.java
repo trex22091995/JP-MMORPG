d09a34d61ea068a80b1a6a234cd7a441ddffa2c2
Jonas <Jonas@VAIO-Jonas>
2014-12-18 22:54:35 +0100

    ModelStore now Static

--- JP - MMORPG/src/Chaos/Util/Misc.java
+++ JP - MMORPG/src/Chaos/Util/Misc.java
@@ -1,6 +1,7 @@
 package Chaos.Util;
 
 public final class Misc {
+	// All System.out.println(); use as this:
 	public static final void log(String msg) {
 		System.out.println("[Info] " + msg);
 	}
--- JP - MMORPG/src/Chaos/Util/Model/ModelStore.java
+++ JP - MMORPG/src/Chaos/Util/Model/ModelStore.java
@@ -3,19 +3,23 @@ package Chaos.Util.Model;
 import java.util.HashMap;
 
 public class ModelStore {
-	private HashMap<String, Model> models = new HashMap<String, Model>();
+	private static HashMap<String, Model> models = new HashMap<String, Model>();
 
-	public void add(String name, Model model) {
-		if (models.containsKey(name)) {
-			models.get(name).destroy();
-			models.remove(name);
+	public static void add(String name, Model model) {
+		if (!models.containsKey(name))
 			models.put(name, model);
-		} else {
-			models.put(name, model);
-		}
 	}
 
-	public Model get(String name) {
-		return models.get(name);
+	public static Model get(String name) {
+		if (models.containsKey(name))
+			return models.get(name);
+		else
+			return null;
+	}
+
+	public static Model rem(String name) {
+		Model model = get(name);
+		models.remove(name);
+		return model;
 	}
 }
