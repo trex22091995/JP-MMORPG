73db6b09cde6383f716973720cf0210bcb7cc845
Jonas <Jonas@VAIO-Jonas>
2014-12-18 23:59:18 +0100

    Methods for Resource Loader

--- JP - MMORPG/src/Chaos/Engine/Resource/Resource.java
+++ JP - MMORPG/src/Chaos/Engine/Resource/Resource.java
@@ -1,6 +1,25 @@
 package Chaos.Engine.Resource;
 
 public class Resource {
-	// For Adding Content to the Game that should be loaded in the Loading
-	// Screen (Textures, Models, Shader)
+	/*
+	 * Die Dateien werden hier NICHT geladen! Es wird nur registriert, was
+	 * geladen werden soll und wie viele es sind (Abschätzungen für den
+	 * Ladebalken) Geladen wird das ganze in einem Thread der parallel zum
+	 * Update-Render Thread läuft, welcher anzeigt wie weit das Laden schon ist.
+	 */
+
+	// Texture Loading
+	public void addTexture(String name, String path) {
+
+	}
+
+	// Model Loading
+	public void addModel(String name, String path) {
+
+	}
+
+	// Shader Loading
+	public void addShader(String name, String path) {
+
+	}
 }
