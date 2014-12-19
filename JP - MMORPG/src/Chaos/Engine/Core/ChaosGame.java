6771e100edf1e3508d10751bb3d0785453e60a3d
Jonas <Jonas@VAIO-Jonas>
2014-12-19 10:22:39 +0100

    Abstract Methods implemented

--- JP - MMORPG/src/Chaos/Engine/Core/ChaosGame.java
+++ JP - MMORPG/src/Chaos/Engine/Core/ChaosGame.java
@@ -4,4 +4,10 @@ import Chaos.Engine.Resource.Resource;
 
 public abstract class ChaosGame {
 	public abstract void loadResources(Resource resources);
+
+	public abstract void update(float delta);
+
+	public abstract void render3D();
+
+	public abstract void render2D();
 }
