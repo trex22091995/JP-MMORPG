59e1926f57e9b52143bc4a0362c4635db877fb88
Jonas <Jonas@VAIO-Jonas>
2014-12-23 00:53:47 +0100

    Improved TestScene (Loading 6 Models, displaying and rotating 1)

--- JP - MMORPG/src/Chaos/Test/Test.java
+++ JP - MMORPG/src/Chaos/Test/Test.java
@@ -5,6 +5,7 @@ import org.lwjgl.opengl.GL11;
 import Chaos.Engine.Core.Chaos;
 import Chaos.Engine.Core.ChaosGame;
 import Chaos.Engine.Core.Resource;
+import Chaos.Util.Model.ModelStore;
 import Chaos.Util.Texture.Text;
 
 public class Test extends ChaosGame {
@@ -14,6 +15,7 @@ public class Test extends ChaosGame {
 	}
 
 	public Test() {
+		// Init Code for the Engine
 		chaos = new Chaos(this);
 		chaos.setScreenSize(800, 600);
 		chaos.setScreenFull(false);
@@ -26,17 +28,33 @@ public class Test extends ChaosGame {
 
 	// Loop
 	public void loadResources(Resource resources) {
-		// TODO Resource.java not fully implemented
-		// resources.addTexture("font", "./Resources/font.png");
+		// Resource Loading (Only Models for now)
+		resources.addModel("Monkey1", "./Resources/untitled.obj");
+		resources.addModel("Monkey2", "./Resources/untitled.obj");
+		resources.addModel("Monkey3", "./Resources/untitled.obj");
+		resources.addModel("Monkey4", "./Resources/untitled.obj");
+		resources.addModel("Monkey5", "./Resources/untitled.obj");
+		resources.addModel("Monkey6", "./Resources/untitled.obj");
 	}
 
+	// Update Loop
 	public void update(float delta) {
+		rot += delta * 90;
+		if (rot >= 360)
+			rot -= 360;
 	}
 
+	float rot = 0;
+
+	// 3D Rendering
 	public void render3D() {
-
+		GL11.glColor3f(0.2f, 0.8f, 0.8f);
+		GL11.glTranslatef(0, 0, -5);
+		GL11.glRotatef(rot, 0, 1, 0);
+		ModelStore.get("Monkey1").draw();
 	}
 
+	// 2D Rendering
 	public void render2D() {
 		GL11.glColor4f(1, 1, 1, 1);
 		GL11.glTranslatef(400, 300, 0);
